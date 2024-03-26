964df1589b1c921d7b9b0532360e165f96910dc9
Moses Mumo <mosesm5499@gmail.com>
2024-03-26 15:20:25 +0300

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- Script that lists all the cities that can be found in the database hbtn_0d_usa
+-- -Script that lists all the cities that can be found in the database hbtn_0d_usa
 SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
 FROM `cities`
 JOIN `states`
