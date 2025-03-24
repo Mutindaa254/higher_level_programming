39bb76eb679e93792e9669c48798ac02a6883d6f
Moses Mumo <mosesm5499@gmail.com>
2025-03-24 22:57:05 +0300

    test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,7 +17,6 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-
 class TestBase_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Base class."""
 
